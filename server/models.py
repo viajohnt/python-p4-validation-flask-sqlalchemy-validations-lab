dc77647074c2e539d51750858e2948f2e31e6cc5
John Truong <viajohnt@gmail.com>
2023-05-28 19:56:10 -0600

    Final commit

--- server/models.py
+++ server/models.py
@@ -11,10 +11,25 @@ class Author(db.Model):
     phone_number = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
+    
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
-
+    
+    @validates('name')
+    def validate_name(self, key, name):
+        names = db.session.query(Author.name).all()
+        if not name:
+            raise ValueError("Must enter name.")
+        elif name in names:
+            raise ValueError("Name must be unique.")
+        return name
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10:
+            raise ValueError ('Number must be exactly 10 digits!')
+        return phone_number
+    
 class Post(db.Model):
     __tablename__ = 'posts'
     # Add validations and constraints 
@@ -27,6 +42,28 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+    
+    @validates('title')
+    def validate_title(self, key, title):
+        clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(substring in title for substring in clickbait):
+            raise ValueError("No clickbait found")
+        return title
+    
+    @validates('content', 'summary')
+    def validate_content(self, key, string):
+        if (key == 'content'):
+            if len(string) <= 250:
+                raise ValueError('Must be at least 250 characters')
+        if (key == 'summary'):
+            if len(string) >= 250:
+                raise ValueError('Maximum of 250 characters')
+        return string
+    
+    @validates('category')
+    def validate_category(self, key, category):
+        if category != 'Fiction' and category != 'Non-Fiction':
+            raise ValueError('Must have a category')
+        return category
